20d38b289bf49dcf61c4aa7fb9724bdafa3d3695
Lyba <faisallyba06@gmail.com>
2024-01-05 16:26:51 +0500

    conditions

---
+++ file2.js
@@ -0,0 +1,27 @@
+let std_marks;
+std_marks=77;
+
+//if/if-else/if-elseif
+//using else if for multiple conditions
+
+if(std_marks>=90)
+{
+    document.write("You have passed with A grade!");
+}
+else if(std_marks>=80)
+{
+    document.write("You have passed with B grade!");
+}
+else if(std_marks>=70)
+{
+    document.write("You have passed with C grade!");
+}
+else if(std_marks>=60)
+{
+    document.write("You have passed with D grade!");
+}
+else 
+{
+    document.write("sorry you are failed!");
+}
+
